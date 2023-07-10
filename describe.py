008c120b66c9c3b212e392c917b276b5168b1bcd
Hiroaki-K4 <49no19@gmail.com>
2023-07-10 16:16:53 +0900

    Update README

--- describe.py
+++ describe.py
@@ -95,4 +95,5 @@ if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Argument is wrong. Please pass the file path as an argument like `describe.py dataset/dataset_train.csv`.")
         exit(1)
+
     main(sys.argv[1])
