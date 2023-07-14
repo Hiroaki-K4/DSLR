080eae7063f7d915e58b67339106cd84260fab18
Hiroaki-K4 <49no19@gmail.com>
2023-07-14 19:07:38 +0900

    Fix format

--- histogram.py
+++ histogram.py
@@ -1,17 +1,18 @@
 import csv
 import sys
+
 import matplotlib.pyplot as plt
 
 
 def main(csv_path: str):
     with open(csv_path) as f:
-        reader = csv.reader(f, delimiter=',')
+        reader = csv.reader(f, delimiter=",")
         input_data_list = [row for row in reader]
 
     header = input_data_list[0]
     numerical_feature_start_pos = 6
     numerical_feature_list = header[numerical_feature_start_pos:]
-    input_data_list = input_data_list[1:len(input_data_list)]
+    input_data_list = input_data_list[1 : len(input_data_list)]
 
     col_num = 5
     if len(numerical_feature_list) % col_num == 0:
@@ -55,9 +56,11 @@ def main(csv_path: str):
     plt.show()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Argument is wrong. Please pass the file path as an argument like `describe.py dataset/dataset_train.csv`.")
+        print(
+            "Argument is wrong. Please pass the file path as an argument like `describe.py dataset/dataset_train.csv`."
+        )
         exit(1)
 
     main(sys.argv[1])
